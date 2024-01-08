208952dae7d9de7945a269de80bd082f8819d0d2
sally wang <sal@LAPTOP-66V3CAS9.localdomain>
2024-01-07 18:26:11 -0800

    connect to database

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ CURR_USER_KEY = "curr_user"
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///recipe_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql:///recipe_db'))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///recipe_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
